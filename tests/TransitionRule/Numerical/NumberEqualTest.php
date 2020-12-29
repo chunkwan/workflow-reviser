25382015c2459ff35958c8a8080c0c2841d27f0b
MaxPronin123 <57519963+MaxPronin123@users.noreply.github.com>
2020-12-29 11:41:45 +0300

    added test for texContains and numberEqual (#8)

---
+++ tests/TransitionRule/Numerical/NumberEqualTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Chunkwan\WorkflowReviser\Tests\TransitionRule\Numerical;
+
+use Chunkwan\WorkflowReviser\Component\TransitionRule\Numerical\NumberEqual;
+use Chunkwan\WorkflowReviser\Tests\WorkflowEventTestCase;
+
+class NumberEqualTest extends WorkflowEventTestCase
+{
+    private Entity $object;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->object = new Entity();
+    }
+
+    public function testWithFilledProperty()
+    {
+        $validation = [
+            'number' => [13, 'error'],
+        ];
+        $this->object->setNumber('13');
+        $event   = $this->getEvent($this->object);
+        $test    = new NumberEqual($event, $this->object, $validation);
+        $test->lookup();
+        $this->assertFalse($event->isBlocked());
+    }
+
+    public function testWithEmptyProperty()
+    {
+        $validation = [
+            'number' => [13, 'Error'],
+        ];
+        $this->object->setNumber('19');
+        $event   = $this->getEvent($this->object);
+        $test    = new NumberEqual($event, $this->object, $validation);
+        $test->lookup();
+        $blockers = $event->getTransitionBlockerList();
+        $this->assertTrue($event->isBlocked());
+        $this->assertEquals(1, $blockers->count());
+        $this->assertStringContainsString('Error', $blockers->getIterator()->current()->getMessage());
+    }
+}
