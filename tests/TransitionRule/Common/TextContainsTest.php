77bf89bf09531dcd534dcee4940f1c64df4cb920
pmg <masiapronin123@gmail.com>
2020-12-29 11:39:50 +0300

    added test for texContains and numberEqual

---
+++ tests/TransitionRule/Common/TextContainsTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Chunkwan\WorkflowReviser\Component\TransitionRule\Common;
+
+use Chunkwan\WorkflowReviser\Tests\TransitionRule\Common\Entity;
+use Chunkwan\WorkflowReviser\Tests\WorkflowEventTestCase;
+
+class TextContainsTest extends WorkflowEventTestCase
+{
+    private Entity $object;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->object = new Entity();
+    }
+
+    public function testWithFilledProperty()
+    {
+        $validations = [
+            'name' => ['watc', 'Error'],
+        ];
+        $this->object->setName('big bro watching you');
+        $event   = $this->getEvent($this->object);
+        $notNull = new TextContains($event, $this->object, $validations);
+        $notNull->lookup();
+        $this->assertFalse($event->isBlocked());
+    }
+
+    public function testWithEmptyProperty()
+    {
+        $validations = [
+            'name'       => ['brother', 'Error'],
+        ];
+        $this->object->setName('big bro watching you');
+        $event   = $this->getEvent($this->object);
+        $notNull = new TextContains($event, $this->object, $validations);
+        $notNull->lookup();
+        $blockers = $event->getTransitionBlockerList();
+        $this->assertTrue($event->isBlocked());
+        $this->assertEquals(1, $blockers->count());
+        $this->assertStringContainsString('Error', $blockers->getIterator()->current()->getMessage());
+    }
+}
---
+++ tests/TransitionRule/Numerical/Entity.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Chunkwan\WorkflowReviser\Tests\TransitionRule\Numerical;
+
+class Entity
+{
+    private int $number = 0;
+
+    /**
+     * @return null|int
+     */
+    public function getNumber(): ?int
+    {
+        return $this->number;
+    }
+
+    /**
+     * @param null|int $number
+     */
+    public function setNumber(?int $number): void
+    {
+        $this->number = $number;
+    }
+}
